a7aeb2423bdb1c73d8b22f277661e55dfd095b6a
Sahil Singh Rajput <90515841+SahilRajputdotexe@users.noreply.github.com>
2024-07-14 00:10:06 +0530

    navigation bar setup

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -1,5 +1,34 @@
-export const NavigationSidebar =() => {
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+
+
+import { NavigationAction } from "./navigation-action";
+
+export const NavigationSidebar =async() => {
+
+    const profile=await currentProfile();
+
+    if(!profile){
+        return redirect('/sign-in');
+    }
+
+
+    const servers =await db.server.findMany({
+        where:{
+            members:{
+                some:{
+                    profileId:profile.id
+                }
+            }
+        }
+    })
+
+
+
     return(
-        <div></div>
+        <div className="space-y-4 flex flex-col items-center h-full  text-primary w-full dark:bg-[#1E1F22] py-3">
+            <NavigationAction />
+        </div>
     )
 }
